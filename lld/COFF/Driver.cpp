90391845db5b0a76cb08dfee4df5c4edd8ec39cb
qfrost <root@qfrost.com>
2022-10-21 13:44:06 +0800

    restore param assert & add release flag for lld

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -256,7 +256,7 @@ void LinkerDriver::enqueuePath(StringRef path, bool wholeArchive, bool lazy) {
       // directory.
       std::string nearest;
       if (optTable.findNearest(pathStr, nearest) > 1)
-        message(msg); //[MSVC Compatibility]
+        error(msg);
       else
         error(msg + "; did you mean '" + nearest + "'");
     } else
